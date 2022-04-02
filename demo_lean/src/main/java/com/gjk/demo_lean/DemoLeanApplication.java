764b16be59b7f5f49f258fb2523bd363f8a03871
gaojiankang <gaojiankang@thunisoft.com>
2022-04-02 18:26:34 +0800

    【*】AOP源码学习，暂时修改

--- demo_lean/src/main/java/com/gjk/demo_lean/DemoLeanApplication.java
+++ demo_lean/src/main/java/com/gjk/demo_lean/DemoLeanApplication.java
@@ -2,12 +2,17 @@ package com.gjk.demo_lean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.gjk.demo_lean.spring.aop.advice.tongyong.TestService;
 
 @SpringBootApplication
 public class DemoLeanApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoLeanApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(DemoLeanApplication.class, args);
+        TestService testService = context.getBean(TestService.class);
+        testService.eatCarrot();
     }
 
 }
