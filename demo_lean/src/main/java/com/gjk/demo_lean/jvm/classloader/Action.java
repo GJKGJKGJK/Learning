b7e6bb445456c37ddf457774a7b3bad25e6f1add
gaojiankang <gaojiankang@thunisoft.com>
2022-04-14 20:08:54 +0800

    【*】修改

--- demo_lean/src/main/java/com/gjk/demo_lean/jvm/classloader/Action.java
+++ demo_lean/src/main/java/com/gjk/demo_lean/jvm/classloader/Action.java
@@ -13,27 +13,23 @@ import java.util.Map;
 public class Action {
 
 
-    public static void main1(String[] args) {
+    public static void main(String[] args) {
         ClassLoader systemLoader = Action.class.getClassLoader();
-        System.out.println("系统类加载器：：：：" + systemLoader);
+        System.out.println("应用程序类加载器：：：：" + systemLoader);
 
         ClassLoader extLoader = systemLoader.getParent();
         System.out.println("扩展类加载器：：：：" + extLoader);
 
         ClassLoader bootLoader = extLoader.getParent();
-        System.out.println("扩展类加载器：：：：" + bootLoader);
+        System.out.println("启动类加载器：：：：" + bootLoader);
 
     }
 
 
-    public static void main(String[] args) {
-//        System.out.println("我是Action,我是被" + Action.class.getClassLoader() + "加载的");
-//        A a = new A();
-//        a.sayHi();
-        Map<String, String> map = new HashMap<>();
-        map.put("123","777");
-        map.put("234","888");
-        System.out.println(map);
+    public static void main1(String[] args) {
+        System.out.println("我是Action,我是被" + Action.class.getClassLoader() + "加载的");
+        A a = new A();
+        a.sayHi();
     }
 
 
